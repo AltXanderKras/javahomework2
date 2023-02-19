c052517c03f63c70c15973667db8884cda5ac07a
Alexander Krasilnikov <i@krasilnikov-1.ru>
2023-02-19 21:48:55 +0300

    Improve

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int TicketPrice = 50_000;
-        int Bonus = 20;
-        int BonusMiles = (TicketPrice / Bonus);
+        int ticketPrice = 50_000;
+        int bonus = 20;
+        int bonusMiles = (ticketPrice / bonus);
 
-        System.out.println("Начисленных бонусных миль:" + BonusMiles);
+        System.out.println("Начисленных бонусных миль:" + bonusMiles);
     }
 }
